dc4c6fcb6fde8e25ac3a95ddbccfb622811a5678
leeboy_0333 <leeboy_0333_0333@163.com>
2018-11-11 22:15:37 +0800

    DiscardHandler与EchoHandler

--- netty001/src/main/java/netty/demo/NettyServer.java
+++ netty001/src/main/java/netty/demo/NettyServer.java
@@ -1,4 +1,4 @@
-package netty.demo.discardserver;
+package netty.demo;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
@@ -8,18 +8,20 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import netty.demo.discardserver.DiscardServerHandler;
+import netty.demo.echoserver.EchoServerHandler;
 
-public class DiscardServer {
+public class NettyServer {
 
     private int port;
 
-    public DiscardServer(int port) {
+    public NettyServer(int port) {
         this.port = port;
     }
 
     public void run() throws Exception {
-        EventLoopGroup bossGroup = new NioEventLoopGroup(); // (1)
-        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1); // (1)
+        EventLoopGroup workerGroup = new NioEventLoopGroup(5);
         try {
             ServerBootstrap b = new ServerBootstrap(); // (2)
             b.group(bossGroup, workerGroup)
@@ -27,10 +29,15 @@ public class DiscardServer {
                     .childHandler(new ChannelInitializer<SocketChannel>() { // (4)
                         @Override
                         public void initChannel(SocketChannel ch) throws Exception {
-                            ch.pipeline().addLast(new DiscardServerHandler());
+                            ch.pipeline()
+//                            .addLast(new LineBasedFrameDecoder(1024))
+//                            .addLast(new StringDecoder())
+                            .addLast(new DiscardServerHandler())
+                            .addLast(new EchoServerHandler());
                         }
                     })
                     .option(ChannelOption.SO_BACKLOG, 128)          // (5)
+//                    .childOption(ChannelOption.AUTO_READ, true)
                     .childOption(ChannelOption.SO_KEEPALIVE, true); // (6)
 
             // Bind and start to accept incoming connections.
@@ -52,6 +59,6 @@ public class DiscardServer {
             port = Integer.parseInt(args[0]);
         }
 
-        new DiscardServer(port).run();
+        new NettyServer(port).run();
     }
 }
--- netty001/src/main/java/netty/demo/discardserver/DiscardServerHandler.java
+++ netty001/src/main/java/netty/demo/discardserver/DiscardServerHandler.java
@@ -3,11 +3,24 @@ package netty.demo.discardserver;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
 
 public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
     @Override
     public  void  channelRead(ChannelHandlerContext  ctx, Object msg){
-        ((ByteBuf)msg).release(); //(3)
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            ByteBuf toBeSend = in.retainedSlice();
+            while (in.isReadable()) { // (1)
+                char b = (char)in.readByte();
+                System.out.print(b);
+            }
+            //在discardServerHandler中仅仅只是记录读取的消息，不作处理，直接下发到后续的Handler
+            ctx.fireChannelRead(toBeSend);
+        } finally {
+            ReferenceCountUtil.release(msg); // (2)
+        }
     }
 
     @Override
